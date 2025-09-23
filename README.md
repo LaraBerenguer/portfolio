64dd81d5a503e1a16038ec71477cd69ce9c32037
LaraBerenguer <laraberenguer@outlook.com>
2025-09-23 13:29:03 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio
+
+Made with React + Vite. Working on it!
+
+<img src=https://media.tenor.com/d7_52xh_2dQAAAAM/dog-work.gif alt="dog working on a computer" width=300>
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Lara Berenguer</h1>
+      <section className="card">
+        <p>
+          Welcome to my page
+        </p>
+      </section>
+      <button>
+        Hellooo
+      </button>
+    </>
+  )
+};
+
+export default App;
