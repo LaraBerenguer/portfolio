11596c1c59176371f91efb770c1617fdcf6a3fba
LaraBerenguer <laraberenguer@outlook.com>
2025-10-14 17:06:24 +0200

    Add footer and fix color name variables and errors in navbar and css

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,16 @@
+import { animateScroll } from "react-scroll";
+
+const Footer = () => {
+    const scrollToTop = () => {
+        animateScroll.scrollToTop();
+    };
+
+    return (
+        <footer className="fixed bottom-0 left-0 right-0 flex justify-between items-center text-sm px-4 py-6 z-10">
+            <p>Made with 💜 by Lara Berenguer</p>
+            <button onClick={scrollToTop} className="rounded-full border-1 border-light text-light px-4 py-2 hover:text-primary hover:border-primary transition-all">↑</button>
+        </footer>
+    );
+};
+
+export default Footer;
