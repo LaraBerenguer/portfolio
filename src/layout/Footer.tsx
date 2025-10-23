6698f367648fecb59c06c086102d4c8ceaf8ffb5
LaraBerenguer <laraberenguer@outlook.com>
2025-10-23 18:42:28 +0200

    Fix fixed footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     };
 
     return (
-        <footer className="fixed bottom-0 left-0 right-0 flex justify-between items-center text-sm px-4 py-6 z-10">
+        <footer className="flex justify-between items-center text-sm px-4 py-6 z-10">
             <p>Made with 💜 by Lara Berenguer</p>
             <button onClick={scrollToTop} className="rounded-full border-1 border-light text-light px-4 py-2 hover:text-primary hover:border-primary transition-all">↑</button>
         </footer>
