915fd14346489be925a845a6189d6fd21d858954
LaraBerenguer <laraberenguer@outlook.com>
2025-10-25 00:15:52 +0200

    Add image import hook for better image management and implement it on ProjectCard.tsx

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,41 +1,40 @@
-import React from 'react';
 import Badge from './Badge';
 import MainButton from './Button';
 import type { Project } from '../../types/types';
+import { useImageImport } from '../../hooks/useImageImport';
 
-interface CardProps {
+interface ProjectCardProps {
     data: Project;
 };
 
-const ProjectCard: React.FC<CardProps> = ({
-    data: {
-        img,
-        title,
-        description,
-        techs,
-        links }
-}) => {
+const ProjectCard = ({ data }: ProjectCardProps) => {
+    const imgSrc = useImageImport(data.img);
 
     return (
         <section className='flex gap-x-8'>
             <div id='card-left max-w-sm rounded-lg overflow-hidden'>
-                <img src={img} className='rounded-lg w-5xl'></img>
+                {imgSrc ?
+                    (<img src={imgSrc} className='rounded-lg w-5xl'></img>)
+                    :
+                    (<div className='rounded-lg w-full h-48 bg-contrast/20 flex items-center justify-center'>
+                        <span className='text-light-muted'>Loading...</span>
+                    </div>)}
             </div>
             <div id='card-right' className='flex flex-col w-full items-start gap-6'>
-                <h3>{title}</h3>
+                <h3>{data.title}</h3>
                 <span className='flex flex-wrap'>
-                    {techs.map(tech =>
+                    {data.techs.map(tech =>
                         <Badge key={tech} skill={tech} />
                     )}
                 </span>
                 <section className='text-start text-sm font-inter'>
-                    {description}
+                    {data.description}
                 </section>
                 <section className='card-buttons flex gap-4'>
-                    <a href={links.demo} target="_blank" rel="noopener noreferrer">
+                    <a href={data.links.demo} target="_blank" rel="noopener noreferrer">
                         <MainButton variant='primary' children={'Visit the site'} />
                     </a>
-                    <a href={links.repo} target="_blank" rel="noopener noreferrer">
+                    <a href={data.links.repo} target="_blank" rel="noopener noreferrer">
                         <MainButton variant='secondary' children={'Repository'} />
                     </a>
                 </section>
